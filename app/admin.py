1139d3b70cd37f333c2c5d2e9464908a9bef490c
sofs44 <sofia.sc.080207@gmail.com>
2025-06-30 16:47:35 -0300

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+from django.contrib import admin
+
+admin.site.register(Usuario)
+admin.site.register(Psicologo)
+admin.site.register(Diario)
+admin.site.register(CheckinEmocional)
+admin.site.register(MetaTerapeutica)
+admin.site.register(FraseMotivacional)
+admin.site.register(Mensagem)
