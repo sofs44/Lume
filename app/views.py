0b7e2a68dd43e068f190122c369b13dd595b8efc
sofs44 <sofia.sc.080207@gmail.com>
2025-07-07 16:59:00 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views import View
+from .models import FraseMotivacional, Diario, CheckinEmocional, MetaTerapeutica, Mensagem
+import random
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        frase_aleatoria = FraseMotivacional.objects.order_by('?').first()
+        if frase_aleatoria:
+            texto_frase = frase_aleatoria.texto
+        else:
+            texto_frase = "Seja bem-vindo ao Lume!"
+
+        return render(request, 'index.html', {"frase": texto_frase})
+
+
+class DiarioView(View):
+    def get(self, request, *args, **kwargs):
+        diarios = Diario.objects.all()
+        return render(request, 'diario.html', {'diarios': diarios})
+
+
+class CheckinView(View):
+    def get(self, request, *args, **kwargs):
+        checkins = CheckinEmocional.objects.all()
+        return render(request, 'checkin.html', {'checkins': checkins})
+
+
+class MetasView(View):
+    def get(self, request, *args, **kwargs):
+        metas = MetaTerapeutica.objects.all()
+        return render(request, 'metas.html', {'metas': metas})
+
+
+class ChatView(View):
+    def get(self, request, *args, **kwargs):
+        mensagens = Mensagem.objects.all()
+        return render(request, 'chat.html', {'mensagens': mensagens})
+
+class ConteudoView(View):
+    template_name = 'conteudo.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django.urls import include, path
-from app.views import *
+from django.urls import path
+from app.views import IndexView, DiarioView, CheckinView, MetasView, ChatView
+from app.views import ConteudoView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('diario/', DiarioView.as_view(), name='diario'),
+    path('checkin/', CheckinView.as_view(), name='checkin'),
+    path('metas/', MetasView.as_view(), name='metas'),
+    path('chat/', ChatView.as_view(), name='chat'),
+    path('conteudo/', ConteudoView.as_view(), name='conteudo'),
 ]
